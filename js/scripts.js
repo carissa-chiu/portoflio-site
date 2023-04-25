c9cedd2b3b80eebd77ffae80e59acb3008d16120
Carissa Chiu <cwchiu3@gmail.com>
2023-04-24 19:56:07 -0700

    Add hamburger menu (javascript code)

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 console.log("Hi there, welcome to my portfolio site! Feel free to poke around and checkout my work.");
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') 
+       {x.className += ' responsive';}
+       else {x.className = 'navtoggle';}
+}
